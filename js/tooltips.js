548c5cbba45811ab7732d5a6ea2235a02b6c94c3
goodrich <susenri@gmail.com>
2025-01-10 20:51:49 +0100

    Update tooltips.js

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,2 +1,4 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
